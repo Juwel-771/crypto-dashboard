05fdc4c86f3253eea59c07845e63524ea913dc8b
Juwel-771 <juwelah99@gmail.com>
2025-05-31 03:43:07 +0600

    Stack - chakra: sidebar create

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,9 +1,35 @@
-import { Box } from '@chakra-ui/react'
+import { Box, HStack, Icon, Stack, Text } from '@chakra-ui/react'
 import React from 'react'
+import { BiSolidDashboard } from "react-icons/bi";
+import { GrTransaction } from "react-icons/gr";
+
+
 
 const Sidenav = () => {
+
+    const navlink = [
+        {
+            icon: BiSolidDashboard,
+            text: "Dashboard",
+            link: "/"
+        },
+        {
+            icon: GrTransaction,
+            text: "Transaction",
+            link: "/transaction"
+        }
+    ]
   return (
-    <Box>Sidenav</Box>
+    <Stack>
+        {
+            navlink.map(nav => (
+                <HStack key={nav.text}>
+                    <Icon as = {nav.icon} />
+                    <Text> {nav.text} </Text>
+                </HStack>
+            ))
+        }
+    </Stack>
   )
 }
 
