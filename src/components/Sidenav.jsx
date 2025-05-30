2d922721183a9bfbf31c2ea965836751ed03ef5b
Juwel-771 <juwelah99@gmail.com>
2025-05-31 03:28:49 +0600

    chakra-ubuntu font added

---
+++ src/components/Sidenav.jsx
@@ -0,0 +1,10 @@
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+
+const Sidenav = () => {
+  return (
+    <Box>Sidenav</Box>
+  )
+}
+
+export default Sidenav
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,9 +1,10 @@
+import Sidenav from '../../components/Sidenav'
 import React from 'react';
 
 const Dashboard = () => {
     return (
         <div>
-            <h2>Dashboard</h2>
+            <Sidenav/>
         </div>
     );
 };
